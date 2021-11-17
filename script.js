c7a58b90e83bf98ade13dd2b938d47215ff19d70
Ritvik Github <89140107+Ritvik-Github@users.noreply.github.com>
2021-11-17 16:29:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//1 kb
+song1 = "";
+song2 = "";
+function preload(){
+    song1 = loadSound("DEAF KEV - Invincible [NCS Release].mp3");
+    song2 = loadSound("Warriyo - Mortals (feat. Laura Brehm) [NCS Release].mp3");
+};
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function start(){
+    song2.play();
+}
