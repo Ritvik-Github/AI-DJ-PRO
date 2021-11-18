c37b9a916e9f6aea60626db06556059ead47054f
Ritvik Github <89140107+Ritvik-Github@users.noreply.github.com>
2021-11-18 15:33:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-song1 = "";
-song2 = "";
+song = [];
 function preload(){
-    song1 = loadSound("DEAF KEV - Invincible [NCS Release].mp3");
-    song2 = loadSound("Warriyo - Mortals (feat. Laura Brehm) [NCS Release].mp3");
+    song[0] = loadSound("DEAF KEV - Invincible [NCS Release].mp3");
+    song[1] = loadSound("Warriyo - Mortals (feat. Laura Brehm) [NCS Release].mp3");
 };
 function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Initialized ✔");
 }
 function draw(){
     image(video,0,0,600,500);
 }
 function start(){
-    song2.play();
+    song[Math.floor(Math.random()*1)].play();
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("==Left Wrist==");
+        console.log("Left Wrist X: " + leftWristX +"Left Wrist Y: " + leftWristY);
+        console.log("==Right Wrist==");
+        console.log("Right Wrist X: " + rightWristX +"Right Wrist Y: " + rightWristY);
+    }
 }
